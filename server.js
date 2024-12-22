6b2fb82ee6a692b96e868e7a9f924746acc71b74
Carlos <carlos.ricardoreiino@gmail.com>
2024-12-22 11:41:21 -0500

    Corregida configuración

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
         user: process.env.MYSQLUSER,
         password: process.env.MYSQLPASSWORD,
         database: process.env.MYSQLDATABASE,
-        port: process.env.MYSQLPORT || 43505,
+        port: process.env.MYSQLPORT,
         ssl: {
             rejectUnauthorized: false
         },
